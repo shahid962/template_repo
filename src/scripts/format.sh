fde2042b12b59ef8610de68312957f539cc00e0a
Jimi Patel <Jimi_Patel@bose.com>
2023-12-26 15:54:11 +0530

    testing code for isort

--- src/scripts/format.sh
+++ src/scripts/format.sh
@@ -9,5 +9,7 @@ if [ "${PARAM_QUITE}" = "true" ]; then
 fi
 echo "Running command: black $V $Q --config ${PARAM_CONFIG} ""${PARAM_PATH}"""
 black $V $Q --config "${PARAM_CONFIG}" "${PARAM_PATH}"
+# Adding a blank line
+echo
 echo "Running command: isort --profile ${PARAM_PROFILE} ""${PARAM_PATH}"""
 isort --profile "${PARAM_PROFILE}" "${PARAM_PATH}"
