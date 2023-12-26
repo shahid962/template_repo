f03f082a61dc1da4f687c0888992ae71b9830139
Jimi Patel <Jimi_Patel@bose.com>
2023-12-26 11:29:24 +0530

    added command for config

--- src/scripts/format.sh
+++ src/scripts/format.sh
@@ -7,5 +7,5 @@ fi
 if [ "${PARAM_QUITE}" = "true" ]; then
     Q="--quiet"
 fi
-echo "Running black $V $Q ""${PARAM_PATH}"" command"
-black $V $Q "${PARAM_PATH}"
+echo "Running black $V $Q --config ${PARAM_CONFIG} ""${PARAM_PATH}"" command"
+black $V $Q --config "${PARAM_CONFIG}" "${PARAM_PATH}"
