47185f4726e3498dfd30ac4588453c9944f21c59
Jimi Patel <Jimi_Patel@bose.com>
2023-12-26 16:56:51 +0530

    testing code for conf file path

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Copy configuration file to given path
+cp "${PARAM_CONFIG}" "${PARAM_PATH}"
 echo "Running command: flake8 --config ${PARAM_CONFIG} ""${PARAM_PATH}"""
 flake8 --config "${PARAM_CONFIG}" "${PARAM_PATH}" --exit-zero
