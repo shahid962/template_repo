95f8a28f64ed608e619836ce5ac854c7dec0a1be
Jimi Patel <Jimi_Patel@bose.com>
2023-12-28 14:07:55 +0530

    testing code for mypy

--- src/scripts/run_mypy.sh
+++ src/scripts/run_mypy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# Copy configuration file to given path
+echo "Installing pip packages..."
+pip install -r "${PARAM_REQUIREMENT_FILE}"
+pip install -r "${PARAM_EXTRA_PACKAGES_FILE}"
+
+# Adding a blank line
+echo
+
 echo "Running command: mypy --config-file ${PARAM_CONFIG} ""${PARAM_PATH}"""
 mypy --config-file "${PARAM_CONFIG}" "${PARAM_PATH}"
