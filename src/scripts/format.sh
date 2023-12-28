750e394446dc28b87b05c98c47d19403526ab30d
Jimi Patel <Jimi_Patel@bose.com>
2023-12-28 12:22:39 +0530

    updated command for black and isort

--- src/scripts/format.sh
+++ src/scripts/format.sh
@@ -10,8 +10,9 @@ if [ "${PARAM_QUIET}" -eq 1 ] ; then
 fi
 
 echo "Running command: black $V $Q --check --config ${PARAM_CONFIG} ""${PARAM_PATH}"""
-black $V $Q --config "${PARAM_CONFIG}" "${PARAM_PATH}"
+black $V $Q --check --config "${PARAM_CONFIG}" "${PARAM_PATH}"
 # Adding a blank line
 echo
 echo "Running command: isort --settings-path ${PARAM_CONFIG} ""${PARAM_PATH}"""
-isort --check-only --settings-path "${PARAM_CONFIG}" "${PARAM_PATH}"
+# isort --check-only --settings-path "${PARAM_CONFIG}" "${PARAM_PATH}"
+isort --settings-path "${PARAM_CONFIG}" "${PARAM_PATH}"
