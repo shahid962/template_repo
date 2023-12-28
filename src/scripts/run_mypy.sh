7b5c301a60f8c4044cb1e4fcefd47c26b406b40f
Jimi Patel <Jimi_Patel@bose.com>
2023-12-28 14:16:41 +0530

    testing code for mypy

--- src/scripts/run_mypy.sh
+++ src/scripts/run_mypy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-echo "Installing pip packages..."
+echo "Installing pip packages from ${PARAM_REQUIREMENT_FILE} file..."
 # Adding a blank line
 echo
 pip install -r "${PARAM_REQUIREMENT_FILE}"
+
+# Install extra packages
+if [ "${PARAM_EXTRA_PACKAGES_FILE}" != "" ]; then
+echo "Installing pip packages from ${PARAM_EXTRA_PACKAGES_FILE} file..."
 pip install -r "${PARAM_EXTRA_PACKAGES_FILE}"
+fi
 
 # Adding a blank line
 echo
